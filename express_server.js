db94e230a0e791559d16037b1c8fb824124ec411
Elham Mireshghi <mireshghi@yahoo.com>
2023-05-23 20:53:40 -0400

    created the ejs template

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+app.set("view engine","ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -15,6 +15,12 @@ app.get("/urls.json", (req, res) => {
 });
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req,res)=> {
+  const templateVars = {urls:urlDatabase};
+  res.render("urls_index",templateVars);
+
 });
 
 app.listen(PORT, () => {
